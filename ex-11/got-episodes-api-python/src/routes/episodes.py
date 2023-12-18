4965e836b5b46fec52508d02c46ff1fd5b6c9f01
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2023-12-18 15:35:23 +0100

    Add missing authorization check on endpoints

--- ex-11/got-episodes-api-python/src/routes/episodes.py
+++ ex-11/got-episodes-api-python/src/routes/episodes.py
@@ -42,18 +42,18 @@ def get_all_episodes(token: str = Depends(get_token_header)):
     return episodes_and_quote
 
 @router.get("/episodes/{episode_id}", response_model=Episode)
-def get_episode(episode_id: str):
+def get_episode(episode_id: str, token: str = Depends(get_token_header)):
     return episodes_controller.get_episode(episode_id)
 
 @router.post("/episodes", response_model=Episode, status_code=status.HTTP_201_CREATED)
-def add_episode(episode: Episode):
+def add_episode(episode: Episode, token: str = Depends(get_token_header)):
     return episodes_controller.add_episode(episode)
 
 @router.put("/episodes/{episode_id}", response_model=Episode)
-def update_episode(episode_id: str, episode: Episode = Body(...)):
+def update_episode(episode_id: str, episode: Episode = Body(...), token: str = Depends(get_token_header)):
     return episodes_controller.update_episode(episode_id, episode)
 
 @router.delete("/episodes/{episode_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_episode(episode_id: str):
+def delete_episode(episode_id: str, token: str = Depends(get_token_header)):
     episodes_controller.delete_episode(episode_id)
     return {"msg": f"Episode with ID {episode_id} has been deleted."}
