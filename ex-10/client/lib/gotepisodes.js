5298244fb484d743cfd0a9e0fe14c31b5daa8d64
Lars Kåre Skjørestad <10708951+larskaare@users.noreply.github.com>
2024-05-21 11:34:12 +0000

    Upd ex-10 dep for client and api. Bump got, update code/tests

--- ex-10/client/lib/gotepisodes.js
+++ ex-10/client/lib/gotepisodes.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const logger = require('./logger.js').logger;
-const got = require('got');  // https://github.com/sindresorhus/got
 const authUtils = require('../lib/auth-utils.js');
 var __ = require('underscore');
 const appConfig = require('./app-config.js'); 
@@ -19,17 +18,19 @@ async function getGOTepisodes(accessToken) {
     try {
         logger.debug('Preparing to read GOT api ...');
         
+        const got = (await import('got')).default;
+
         const url = appConfig.gotApiUrl + '/api/episodes';
         const response = await got.get(
             url,
             {
                 headers: requestHeaders,
-                retry: 0   //Number of retries in case of error - including ratelimiting
+                retry: { limit: 0}, //Number of retries in case of error - including rate limiting
+                responseType: 'json'
             }
         );
-   
 
-        var episodesBody = JSON.parse(response.body);
+        var episodesBody = response.body;
  
         __.each(episodesBody, function (item, index) {
             episodes.push(item.id + ' - ' + item.title);
