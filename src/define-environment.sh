f46fe72f6826eb2eaed5de3af2baa589bc12c2ce
Stein A Sivertsen <steinsiv@users.noreply.github.com>
2024-11-12 08:05:46 +0100

    Codespace improvements (#40)
    * re-arrange scripts and install pre-commit
    
    * finetune
    
    * migrate pre-commit config
    
    * remove unused post-createcommand

--- src/define-environment.sh
+++ src/define-environment.sh
@@ -3,3 +3,4 @@
 echo 'export PATH=$CODESPACE_VSCODE_FOLDER/src:$PATH' >> ~/.zshrc
 sed -i 's/ZSH_THEME="devcontainers"/ZSH_THEME="avit"/' ~/.zshrc
 sudo apt update -qq && sudo apt install -y -qq cloc
+cd "$CODESPACE_VSCODE_FOLDER" && pre-commit install
